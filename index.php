6c4356074af3a97d56fb2bcb007045ad46716eca
Kevinfame007 <53188436+Kevinfame007@users.noreply.github.com>
2021-12-16 17:18:01 +0700

    update footer header register_news
    update footer header register_news

--- index.php
+++ index.php
@@ -13,14 +13,18 @@
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link rel="stylesheet" href="style.css">
-    <title>MAZUMA</title>
+    <title>หน้าหลัก | HOME PAGE</title>
+
 </head>
 
 <body>
-    <div class="container-fluid">
-        <div class="row">
+    <!-- Navigation -->
+    <?php include('includes/header.php'); ?>
+
+    <div class="container">
+        <div class="row" style="margin-top: 4%">
             <!-- ไลด์ข่าวสำคัญ -->
-            <div class="col bg-warning pb-3 ">
+            <div class="col bg-warning pb-3">
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-indicators">
                         <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -79,13 +83,18 @@
                     </div>
                 </div>
             </div>
-
         </div>
     </div>
+
+
+    <!-- Footer -->
+    <?php include('includes/footer.php'); ?>
 
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+
+
 </body>
 
 </html>
--- user.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE usertbl (
-    id INT(5) NOT NULL PRIMARY KEY,
-    username VARCHAR(50) NOT NULL,
-    password VARCHAR(200) NOT NULL,
-    firstname VARCHAR(100) NOT NULL,
-    lastname VARCHAR(100) NOT NULL,
-    userlevel VARCHAR (100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
