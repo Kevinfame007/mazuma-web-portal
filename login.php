0489b727050642d64e5fb0690fad0bdfd37f40e3
Kevinfame007 <53188436+Kevinfame007@users.noreply.github.com>
2021-12-16 17:36:02 +0700

    update

--- login.php
+++ login.php
@@ -1,6 +1,6 @@
-
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
   <meta charset="UTF-8">
@@ -10,33 +10,36 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
   <link rel="stylesheet" href="style.css" />
   <link rel="stylesheet" href="https://pro.fontawesome.com/releases/v5.10.0/css/all.css" integrity="sha384-AYmEC3Yw5cVb3ZcuHtOA93w35dYTsvhLPVnYs9eStHfGJvOvKxVfELGroGkvsg+p" crossorigin="anonymous" />
+
+</head>
+
+<body>
+  <nav class="navbar navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="index.php">
+        <img src="images/Asset-1@4x.png" alt="" width="210" height="46">
+      </a>
+    </div>
+  </nav>
+
+  <div class="container-fluid">
+    <form action="login.php" method="post">
+      <label for="username">ชื่อผู้ใช้</label>
+      <input type="text" name="username" placeholder="Username" require>
+      <br>
+      <label for="password">รหัสผ่าน</label>
+      <input type="password" name="password" placeholder="Password" require>
+      <br>
+      <input type="submit" name="submit" value="Login">
+    </form>
+
+    <a href="register.php">Go to register</a>
+  </div>
+
   <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 
-</head>
-<body>
-<nav class="navbar navbar-light bg-light">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="index.php">
-      <img src="images/Asset-1@4x.png" alt="" width="210" height="46">
-    </a>
-  </div>
-</nav>
-
-<div class="container-fluid">
-<form action="login.php" method="post">
-  <label for="username">ชื่อผู้ใช้</label>
-  <input type="text" name="username" placeholder="Username" require>
-  <br>
-  <label for="password">รหัสผ่าน</label>
-  <input type="password" name="password" placeholder="Password" require>
-  <br>
-  <input type="submit" name="submit" value="Login">
-</form>
-
-<a href="register.php">Go to register</a>
-</div>
-
 </body>
+
 </html>
