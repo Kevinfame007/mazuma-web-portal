404824bc7f8fb4a61102c0f9fa578437728e615b
Kevinfame007 <53188436+Kevinfame007@users.noreply.github.com>
2021-12-16 15:12:29 +0700

    sprit file

---
+++ includes/header.php
@@ -0,0 +1,49 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+
+    <a class="navbar-brand" href="index.php">
+        <img src="images/logo.png" height="53">
+    </a>
+
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="index.php">หน้าหลัก</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">การรับสมัคร</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">ระบบสารสนเทศออนไลน์</a>
+            </li>
+
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    หน่วยงาน
+                </a>
+                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                    <li><a class="dropdown-item" href="#">หน่วยที่ 1</a></li>
+                    <li><a class="dropdown-item" href="#">หน่วยที่ 2</a></li>
+                </ul>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">เบอร์ติดต่อภายใน</a>
+            </li>
+
+            <li class="nav-item">
+                <a class="nav-link" href="#">ปฏิทินกิจกรรม</a>
+            </li>
+            
+        </ul>
+
+    </div>
+
+</nav>
--- index.php
+++ index.php
@@ -19,48 +19,6 @@
 <body>
     <div class="container-fluid">
         <div class="row">
-            <!-- แทบด้านบน -->
-            <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top pt-4 pb-4 ps-5">
-                <div class="col-5">
-                    <a class="navbar-brand" href="index.php">
-                        <img src="images/Asset-1@4x.png" alt="" width="240" height="53">
-                    </a>
-                </div>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="index.php">หน้าหลัก</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">การรับสมัคร</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">ระบบสารสนเทศออนไลน์</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                หน่วยงาน
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">หน่วยที่ 1</a></li>
-                                <li><a class="dropdown-item" href="#">หน่วยที่ 2</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">เบอร์ติดต่อภายใน</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">ปฏิทินกิจกรรม</a>
-                        </li>
-                    </ul>
-                </div>
-            </nav>
-            <!-- จบแทบด้านบน -->
-
             <!-- ไลด์ข่าวสำคัญ -->
             <div class="col bg-warning pb-3 ">
                 <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
