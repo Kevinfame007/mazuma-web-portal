44aa599dccbf4245ed30c16ad4476d2319dd245b
Kevinfame007 <53188436+Kevinfame007@users.noreply.github.com>
2021-12-23 14:32:15 +0700

    update admin index.php

---
+++ admin/includes/footer_admin.php
@@ -0,0 +1,5 @@
+<footer class="py-3 bg-transparent" style="margin-top: 250px;">
+    <div class="container-fuild">
+        <p class="m-0 text-center text-white">Copyright &copy; Template Code From codecamps.com Edit Suphanut Taengtoemphon 2021 - 2022</p>
+    </div>
+</footer>
