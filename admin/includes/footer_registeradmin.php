d211eab99c185d50ab4b625619b8c8c14e42f85f
Kevinfame007 <53188436+Kevinfame007@users.noreply.github.com>
2022-01-06 10:45:57 +0700

    update all stuff

---
+++ admin/includes/footer_registeradmin.php
@@ -0,0 +1,5 @@
+<footer class="py-3 bg-transparent" style="margin-top: 50px;">
+    <div class="container-fuild pt-1">
+        <p class="m-0 text-center text-white">Copyright &copy; Template Code From codecamps.com Edit Suphanut Taengtoemphon 2021 - 2022</p>
+    </div>
+</footer>
